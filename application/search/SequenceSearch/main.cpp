e3cd0a8ed1befc2543545e6da7d38f2d1b4db168
Tongjiangyong <1246376353@qq.com>
2017-08-29 23:53:23 +0800

    add SequenceSearch

--- application/search/SequenceSearch/main.cpp
+++ application/search/SequenceSearch/main.cpp
@@ -18,16 +18,13 @@ template <class T>
 int getArrayLen(T& array)
 
 {
-
-return (sizeof(array) / sizeof(array[0]));
-
+    //数组作为参数传给函数时传的是指针而不是数组，传递的是数组的首地址
+    //如果这里不是引用，则永远都是1，一定要注意即可....
+    return (sizeof(array) / sizeof(array[0]));
 }
-//顺序查找
-int SequenceSearch(int a[], int value)
+//在C/C++中，如果传递到是指针，则一般无法计算数组长度，strlen是例外.....清楚原理即可....
+int SequenceSearch(int a[],int value,int n)
 {
-    int n;
-    n=getArrayLen(a);
-    cout<<"len_1 is "<<n<<endl;
     int i;
     for(i=0; i<n; i++)
         if(a[i]==value)
@@ -43,19 +40,19 @@ int main(){
 #endif
 int num;
 cin>>num;
+//注意这里可以知道，动态数组的长度，没法直接使用len的方式求得
+//因为在C语言中，指针和数组是不同的....这一点还是要记住的.....
 int *arrayInt = new int[num];
+int test[] ={1,2,6,8};
 for(int i=0;i<num;i++)
 {
     cin>>arrayInt[i];
+    cout<<arrayInt[i]<<" ";
 }
+cout<<endl;
 int searchNum;
 cin>>searchNum;
-int n;
-    n=getArrayLen(arrayInt);
-    cout<<"len_2 is "<<n<<endl;
-cout<<SequenceSearch(arrayInt,searchNum)<<endl;
-
-
+cout<<SequenceSearch(arrayInt,searchNum,num)<<endl;
 #ifdef LOCAL
     fclose(stdin);
     fclose(stdout);
